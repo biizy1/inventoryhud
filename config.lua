09f0df7015b68bce3697c69bc5dc43278028e210
biizy1 <62166408+biizy1@users.noreply.github.com>
2020-03-14 01:13:41 -0400

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,47 @@
+Config = {}
+Config.Locale = "en"
+Config.IncludeCash = true -- Include cash in inventory?
+Config.IncludeWeapons = true -- Include weapons in inventory?
+Config.IncludeAccounts = true -- Include accounts (bank, black money, ...)?
+Config.ExcludeAccountsList = {"bank"} -- List of accounts names to exclude from inventory
+Config.OpenControl = 289 -- Key for opening inventory. Edit html/js/config.js to change key for closing it.
+
+-- List of item names that will close ui when used
+Config.CloseUiItems = {
+    
+    "advancedlockpick",
+    'armor',
+    "bandage",
+    "bandage2",
+    "beer",
+    "binoculars",
+    "blowpipe",
+    "carokit",
+    "carotool",
+    "clip",
+    "coffee",
+    "coke",
+    "fishbait",
+    "fishingrod",
+    "fixkit",
+    "fixtool",
+    "fountain",
+    "gazbottle",
+    "handcuffs",
+    "lockpick",
+    "lsd",
+    "marijuana",
+    "medikit",
+    "monster",
+    "oxygen_mask",
+    "rope",
+    "shotburst",
+    "spice",
+    "starburst",
+    "trailburst",
+    "tuning_laptop",
+    "turtlebait",
+    "vodka",
+	"notepad"
+
+}
